e61b320a4008de015a0d0491af5129626685708e
amirfoox@gmail.com <amirfoox@gmail.com>
2023-02-20 09:45:12 +0200

    New

---
+++ string_methods.py
@@ -0,0 +1,17 @@
+# # string methods examples
+#
+# # which are the string methods?
+#
+# print(dir("xx"))
+#
+# a = "hello"
+# print(a.capitalize())
+# a = "HELLO"
+# print(a.capitalize())  # makes only the first letter capitalized, and the the rest lower
+
+help(help("".center))
+
+print("Amir".center(50))
+print("Amir".center(50, "!"))
+
+
