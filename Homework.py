fe00c903a6a072667b04570cf3e3436b8b0d6382
amirfoox@gmail.com <amirfoox@gmail.com>
2023-02-27 13:00:13 +0200

    Lesson4

---
+++ Homework.py
@@ -0,0 +1,40 @@
+import random
+
+# def sum10():
+#     numbers = [random.randint(0, 100) for i in range(10)]
+#     total = sum(numbers)
+#     print("The 10 random numbers are: {}".format(numbers))
+#     print("Their sum is: {}".format(total))
+#
+#
+# sum10()
+
+# def odd_or_even(number, odd=True):
+#     if odd:
+#         if number % 2 == 0:
+#             print(f"{number} is not odd")
+#         else:
+#             print(f"{number} is odd")
+#     else:
+#         if number % 2 == 0:
+#             print(f"{number} is even")
+#         else:
+#             print(f"{number} is not even")
+#
+#
+# odd_or_even(int(input("Enter a number: ")))
+
+
+# def generate_random_list(n):
+#     random_list = []
+#     for i in range(n):
+#         random_list.append(random.randint(0, 100))
+#     return random_list
+#
+#
+# my_random_list = generate_random_list(int(input("Enter amount of numbers to generate: ")))
+# print(my_random_list)
+
+
+
+
